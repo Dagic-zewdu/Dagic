f89f12a034964ad022f5bcee286a3754ffc6c1b5
Dagi <dagi.zewdu.dz@gmail.com>
2022-08-01 17:01:43 +0300

    Added title of portifolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,7 +7,7 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Dagic zewdu",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
