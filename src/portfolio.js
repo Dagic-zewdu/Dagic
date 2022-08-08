b183015f5dab6b165fa95481d16d1e45fc7c51e2
Dagi <dagi.zewdu.dz@gmail.com>
2022-08-08 19:00:08 +0300

    Some minor fixes

--- src/portfolio.js
+++ src/portfolio.js
@@ -357,7 +357,7 @@ const certifications = {
 // Experience Page
 const experience = {
   title: "Experience",
-  subtitle: "Work, Internship and Volunteership",
+  subtitle: "Work and Volunteership",
   description:
     "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
   header_image_path: "experience.svg",
