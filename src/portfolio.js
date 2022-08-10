b341f53d2bec762b9e74c7af7ecfab71410b5f66
Dagi <dagi.zewdu.dz@gmail.com>
2022-08-10 20:21:43 +0300

    Added contact page

--- src/portfolio.js
+++ src/portfolio.js
@@ -432,7 +432,7 @@ const publicationsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "animated_ashutosh.png",
+    profile_image_path: "profile.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
@@ -440,19 +440,18 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://dagic-zewdu.wordpress.com",
+    link: "https://almondine-sideways-telephone.glitch.me/",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
     title: "Address",
-    subtitle:
-      "Ambavadi vas, Kanodar, T.A.-Palanpur, Dist.-Banaskantha, Gujarat - 385520",
+    subtitle: "Ethiopia, Addis Ababa",
     avatar_image_path: "address_image.svg",
-    location_map_link: "https://goo.gl/maps/MpMqtRZytFchMkZ76",
+    location_map_link: "https://goo.gl/maps/kCE4mZ8Cij1rmx1E9",
   },
   phoneSection: {
     title: "Phone Number",
-    subtitle: "+91 8320758513",
+    subtitle: "+251 910565994",
   },
 };
 
